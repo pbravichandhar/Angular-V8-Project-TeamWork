ad737bbdff4252c374c48fce1fdc6c1032f9b5de
Tharini <tharini@calibraint.com>
2019-12-10 13:02:28 +0530

    Work on mat stackbar

--- Tharani-project/angular-application/src/app/core/material.module.ts
+++ Tharani-project/angular-application/src/app/core/material.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import {
     MatButtonModule, MatNativeDateModule, MatIconModule, MatSidenavModule, MatListModule, MatToolbarModule
 } from '@angular/material';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
-    imports: [CommonModule, MatButtonModule, MatToolbarModule, MatNativeDateModule, MatIconModule, MatSidenavModule, MatListModule],
-    exports: [CommonModule, MatButtonModule, MatToolbarModule, MatNativeDateModule, MatIconModule, MatSidenavModule, MatListModule],
+    imports: [CommonModule, MatButtonModule, MatToolbarModule, MatNativeDateModule, MatIconModule, MatSidenavModule, MatListModule, MatSnackBarModule,
+    ],
+    exports: [CommonModule, MatButtonModule, MatToolbarModule, MatNativeDateModule, MatIconModule, MatSidenavModule, MatListModule, MatSnackBarModule,
+    ],
 })
 export class CustomMaterialModule { }
--- Tharani-project/angular-application/src/app/file/file.component.ts
+++ Tharani-project/angular-application/src/app/file/file.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-file',
-  templateUrl: './file.component.html'
-  // styleUrls: ['./file.component.css']
+  templateUrl: './file.component.html',
+  styleUrls: ['./file.component.css']
 })
 export class FileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000
+    })
   }
 
 }
